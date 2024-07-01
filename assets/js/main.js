eec55fc47b204c10c614137e0a53dc545c6284cf
alvinoDev <48383924+alvinoDev@users.noreply.github.com>
2024-07-01 18:55:22 -0400

    Feature: Add Header and Nav bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,31 @@
 
 // LOADER
 
+const navMenu = document.getElementById('nav_menu'),
+	navToggle = document.getElementById('nav_toogle'),
+	navClose = document.getElementById('nav_close');
 // SHOW MENU
+if (navToggle) {
+	navToggle.addEventListener('click', () => {
+		navMenu.classList.add('show_menu');
+	});
+}
+// HIDDEN MENU
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show_menu');
+	});
+}
 
 // REMOVE MENU MOBILE
+const navLink =document.querySelectorAll('.nav_link');
+
+function linkAction () {
+    const navMenu = document.getElementById('nav_menu');
+    navMenu.classList.remove('show_menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
 
 // CHANGE BACKGROUND HEADER
 
